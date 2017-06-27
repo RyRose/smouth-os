8d37ed0772f33858c6e5abc2d48443c21c1f9c88
Ryan Rose <ryanthrose@gmail.com>
2017-06-26 22:47:06 -0500

    Loads the GDT into memory with 3 entries upon kernel load.

---
+++ kernel/include/kernel/gdt.h
@@ -0,0 +1,8 @@
+#ifndef _KERNEL_GDT_H
+#define _KERNEL_GDT_H
+
+#include <stdint.h>
+
+uint64_t installGdt();
+
+#endif
--- kernel/kernel/kernel.cc
+++ kernel/kernel/kernel.cc
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 #include <kernel/tty.h>
+#include <kernel/gdt.h>
 
 extern "C"
 void kernel_main(void) {
   terminal_initialize();
   printf("Hello, World!\n");
+  installGdt();
 }
