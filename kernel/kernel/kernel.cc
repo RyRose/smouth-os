a74c83a812ca389c96c399b9c5f1d8df7646ab94
Ryan Rose <ryanthrose@gmail.com>
2017-06-27 23:44:27 -0500

    Enabled protected mode.

--- kernel/kernel/kernel.cc
+++ kernel/kernel/kernel.cc
@@ -7,5 +7,6 @@ extern "C"
 void kernel_main(void) {
   terminal_initialize();
   printf("Hello, World!\n");
-  installGdt();
+  uint64_t gdt_ptr = installGdt();
+  printf("Enabled gdt and protected mode. gdt_ptr: %X.\n", gdt_ptr);
 }
