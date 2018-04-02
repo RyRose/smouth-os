1d1f8403749c4a22d76e6fdbe874a60ef7b54401
Ryan Rose <ryanthrose@gmail.com>
2018-04-01 23:59:07 -0700

    Add port IO for i386.
      This commit adds a header file for accessing the IO ports along with an
    implementation for i386. Port IO is needed for interfacing with any old
    devices and there's a really handy debug port at 0xE9 that can be used
    for logging.

---
+++ kernel/arch/i386/io_port/io_port.cc
@@ -0,0 +1,49 @@
+#include "kernel/arch/io_port.h"
+
+#include <stdint.h>
+
+namespace arch {
+
+void IoPort::outb(uint8_t output) {
+  asm volatile ("outb %0, %1" : /* No outputs */
+                              : "r" (output), "r" (port_number_)
+      );
+}
+
+void IoPort::outw(uint16_t output) {
+  asm volatile ("outw %0, %1" : /* No outputs */
+                              : "r" (output), "r" (port_number_)
+      );
+}
+
+void IoPort::outl(uint32_t output) {
+  asm volatile ("outl %0, %1" : /* No outputs */
+                              : "r" (output), "r" (port_number_)
+      );
+}
+
+uint8_t IoPort::inb() {
+  uint8_t ret;
+  asm volatile ("inb %w1, %0" : "=r" (ret)
+                              : "Nd" (port_number_)
+      );
+  return ret;
+}
+
+uint16_t IoPort::inw() {
+  uint16_t ret;
+  asm volatile ("inw %1, %0" : "=r" (ret)
+                             : "Nd" (port_number_)
+      );
+  return ret;
+}
+
+uint32_t IoPort::inl() {
+  uint32_t ret;
+  asm volatile ("inl %1, %0" : "=r" (ret)
+                             : "Nd" (port_number_)
+      );
+  return ret;
+}
+
+}
