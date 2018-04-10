aff0340c121dbbe78565c412cb503ec1dabee2d4
Ryan Rose <ryanthrose@gmail.com>
2018-04-09 23:20:42 -0700

    Adding convenience shortcuts for running/testing the code.

--- qemu.py
+++ qemu.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 import os
 
+AVAILABLE_ARCHITECTURES=["i386"]
+
 def run_qemu(arch, kernel):
   subprocess.check_call(["qemu-system-{arch}".format(arch=arch),
                          "-kernel",
@@ -15,7 +17,8 @@ def run_qemu(arch, kernel):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument("--arch", help="The guest architecture to run with.")
+  parser.add_argument("--arch", help="The guest architecture to run with.",
+      choices=AVAILABLE_ARCHITECTURES)
   parser.add_argument("--kernel", help="Path to the kernel to boot.")
   args = parser.parse_args()
   run_qemu(args.arch, args.kernel)
