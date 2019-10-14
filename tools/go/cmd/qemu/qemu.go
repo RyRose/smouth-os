382b52c4fa5f3b9f480c67aaa89a452c45068a80
Ryan Rose <ryanthrose@gmail.com>
2019-10-13 17:05:22 -0700

    Fix terminal weirdness with ctrl+c

--- tools/go/cmd/qemu/qemu.go
+++ tools/go/cmd/qemu/qemu.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 	"strings"
 	"syscall"
@@ -48,6 +49,7 @@ func run(ctx context.Context, vm qemu.VM) error {
 func main() {
 	flag.Parse()
 	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
@@ -61,7 +63,17 @@ func main() {
 		Workspace: workspace(),
 	}
 
-	if err := run(ctx, vm); err != nil {
-		log.Fatalf("Run failed: %v", err)
+	if err := run(ctx, vm); err != nil && ctx.Err == nil {
+		log.Fatalf("QEMU execution failed: %v", err)
+	}
+
+	// If context is canceled, we likely exited early. This messes up terminals so we
+	// execute `stty sane` to fix the weirdness.
+	if ctx.Err() != nil {
+		stty := exec.Command("stty", "sane")
+		stty.Stdin = os.Stdin
+		stty.Stdout = os.Stdout
+		stty.Stderr = os.Stderr
+		stty.Run()
 	}
 }
