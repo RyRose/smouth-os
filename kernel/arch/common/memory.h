6006d071fa23540fb87db3d94868d6b72091ae66
Ryan Rose <ryanthrose@gmail.com>
2020-09-26 15:17:30 -0700

    Add interrupts ktest, ret_checkf in util::List, and MemoryRegion default initialization.

--- kernel/arch/common/memory.h
+++ kernel/arch/common/memory.h
@@ -7,18 +7,17 @@
 
 namespace arch {
 
-enum class MemoryRegionType { AVAILABLE, RESERVED };
+enum class MemoryRegionType { UNKNOWN, AVAILABLE, RESERVED };
 
 struct MemoryRegion {
  public:
   MemoryRegion(uint64_t address_, uint64_t length_, MemoryRegionType type_)
       : address(address_), length(length_), type(type_){};
-
   MemoryRegion() = default;
 
-  uint64_t address;
-  uint64_t length;
-  MemoryRegionType type;
+  uint64_t address = 0;
+  uint64_t length = 0;
+  MemoryRegionType type = MemoryRegionType::UNKNOWN;
 };
 
 }  // namespace arch
