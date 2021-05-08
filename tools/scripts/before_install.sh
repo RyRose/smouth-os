d01a7fca615601c2e53e1d75a852ac362094e545
RyRose <ryanthrose@gmail.com>
2021-05-08 15:29:12 -0700

    Refactor more out of scripts to travis CI matrix.

--- tools/scripts/before_install.sh
+++ tools/scripts/before_install.sh
@@ -2,20 +2,14 @@
 
 set -eux
 
+BAZEL_VERSION="${1}"
+BAZEL_OS="${2}"
+SHA256SUM="${3}"
+
 # Download Bazel install script and verify matches checksum.
 installer="bazel-${BAZEL_VERSION}-installer-${BAZEL_OS}-x86_64.sh"
 wget "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${installer}"
-case "${BAZEL_OS}" in
-  darwin)
-    shasum -a 256 -c "tools/checksums/${installer}.sha256"
-    ;;
-  linux)
-    sha256sum --check "tools/checksums/${installer}.sha256"
-    ;;
-  *)
-    echo "${BAZEL_OS} is not a supported OS."
-    exit 1
-esac
+"${SHA256SUM}" "tools/checksums/${installer}.sha256"
 
 # Install Bazel.
 chmod +x "${installer}"
