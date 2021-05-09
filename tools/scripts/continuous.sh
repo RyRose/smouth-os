697cb88cdfdb5296488c5ae6a21220d11b82490a
RyRose <ryanthrose@gmail.com>
2021-05-08 20:32:50 -0700

    Restructure travis CI build.

--- tools/scripts/continuous.sh
+++ tools/scripts/continuous.sh
@@ -2,9 +2,14 @@
 
 set -eux;
 
-CONFIG="${1}"
+ACTION="${1}"
+CONFIG="${2}"
 
-bazel test \
+bazel "${ACTION}" \
+  --test_strategy standalone \
+  --genrule_strategy standalone \
+  --output_base "${HOME}/.cache/bazel/output" \
+  --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
   --config ci \
   --config "${CONFIG}" \
   --  //...
