7bd475e9e322860a942f1ed86575055492afa517
RyRose <ryanthrose@gmail.com>
2021-05-08 22:17:42 -0700

    Fix buggy run_bazel shell script.

--- tools/scripts/run_bazel.sh
+++ tools/scripts/run_bazel.sh
@@ -6,30 +6,20 @@ ACTION="${1:-test}"
 CONFIG="${2:-i386}"
 CACHE="${3:-false}"
 
-function run_bazel_config() {
-  config="${1}"
-
-  if [ "${CACHE}" = "true" ]; then
-    bazel \
-      --output_base "${HOME}/.cache/bazel/output" \
-      "${ACTION}" \
-      --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
-      --test_strategy standalone \
-      --genrule_strategy standalone \
-      --config ci \
-      --config "${config}" \
-      --  //...
-    return
-  fi
-
-  bazel "${ACTION}" \
+if [ "${CACHE}" = "true" ]; then
+  bazel \
+    --output_base "${HOME}/.cache/bazel/output" \
+    "${ACTION}" \
+    --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
+    --test_strategy standalone \
+    --genrule_strategy standalone \
     --config ci \
-    --config "${config}" \
+    --config "${CONFIG}" \
     --  //...
-}
+  exit
+fi
 
-function main() {
-  run_bazel_config "${CONFIG}"
-}
-
-main
+bazel "${ACTION}" \
+  --config ci \
+  --config "${CONFIG}" \
+  --  //...
