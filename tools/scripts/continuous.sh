031468887f5aa05cefed174f6564b8a4661e2c0c
RyRose <ryanthrose@gmail.com>
2021-05-08 21:05:47 -0700

    Re-arrange Travis CI bazel command.

--- tools/scripts/continuous.sh
+++ tools/scripts/continuous.sh
@@ -5,11 +5,12 @@ set -eux;
 ACTION="${1}"
 CONFIG="${2}"
 
-bazel "${ACTION}" \
-  --test_strategy standalone \
-  --genrule_strategy standalone \
+bazel
   --output_base "${HOME}/.cache/bazel/output" \
   --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
+  "${ACTION}" \
+  --test_strategy standalone \
+  --genrule_strategy standalone \
   --config ci \
   --config "${CONFIG}" \
   --  //...
