a5b181ee9ce2e7c670b506d5f611446000f0578b
RyRose <ryanthrose@gmail.com>
2021-05-08 22:00:02 -0700

    Refactor run_bazel logic into functions.

--- tools/scripts/run_bazel.sh
+++ tools/scripts/run_bazel.sh
@@ -6,20 +6,30 @@ ACTION="${1:-test}"
 CONFIG="${2:-i386}"
 CACHE="${3:-false}"
 
-if [ "${CACHE}" = "true" ]; then
-  bazel \
-    --output_base "${HOME}/.cache/bazel/output" \
-    "${ACTION}" \
-    --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
-    --test_strategy standalone \
-    --genrule_strategy standalone \
-    --config ci \
-    --config "${CONFIG}" \
-    --  //...
-  exit
-fi
+function run_bazel_config() {
+  config="${1}"
 
-bazel "${ACTION}" \
-  --config ci \
-  --config "${CONFIG}" \
-  --  //...
+  if [ "${CACHE}" = "true" ]; then
+    bazel \
+      --output_base "${HOME}/.cache/bazel/output" \
+      "${ACTION}" \
+      --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
+      --test_strategy standalone \
+      --genrule_strategy standalone \
+      --config ci \
+      --config "${config}" \
+      --  //...
+    return
+  fi
+
+  bazel "${ACTION}" \
+    --config ci \
+    --config "${config}" \
+    --  //...
+}
+
+function main() {
+  run_bazel_config "${CONFIG}"
+}
+
+main
