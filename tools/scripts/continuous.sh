40f53aa2f25cfe5da86ff04197270e20e2997182
RyRose <ryanthrose@gmail.com>
2021-05-08 21:09:51 -0700

    Fix bug in bazel command.

--- tools/scripts/continuous.sh
+++ tools/scripts/continuous.sh
@@ -5,8 +5,7 @@ set -eux;
 ACTION="${1}"
 CONFIG="${2}"
 
-bazel
-  --output_base "${HOME}/.cache/bazel/output" \
+bazel --output_base "${HOME}/.cache/bazel/output" \
   --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
   "${ACTION}" \
   --test_strategy standalone \
