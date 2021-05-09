c6ff74dec0c39a98e2c27a60bf31c40f508cde2f
RyRose <ryanthrose@gmail.com>
2021-05-08 21:22:07 -0700

    Fix bug in bazel command and add defaults to scripts.

---
+++ tools/scripts/run_bazel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -eux;
+
+ACTION="${1:-test}"
+CONFIG="${2:-i386}"
+CACHE="${3:-false}"
+
+if [ ${CACHE} == "true" ]; then
+bazel \
+  --output_base "${HOME}/.cache/bazel/output" \
+  "${ACTION}" \
+  --experimental_repository_cache "${HOME}/.bazel_repository_cache" \
+  --test_strategy standalone \
+  --genrule_strategy standalone \
+  --config ci \
+  --config "${CONFIG}" \
+  --  //...
+else
+bazel "${ACTION}" \
+  --config ci \
+  --config "${CONFIG}" \
+  --  //...
+fi
