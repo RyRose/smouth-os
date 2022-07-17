6079ccd9ea6b8b8f4a7ab613ec2bd029fa2488b8
RyRose <RyRose@users.noreply.github.com>
2022-07-16 22:59:15 -0700

    Add rtsc instruction to i386 with a ktest.

--- kernel/arch/i386/instructions/instructions.h
+++ kernel/arch/i386/instructions/instructions.h
@@ -16,16 +16,16 @@ inline util::Status LIDT(uint64_t idtr_value) {
                 idtr_value);
   // TODO(RyRose): Check that CPL = 0. The current privilege level (CPL) must be
   // zero for this instruction to work.
-  asm volatile("LIDT %0" ::"m"(idtr_value));
+  asm volatile("LIDT %0"::"m"(idtr_value));
   return {};
 }
 
 // INT calls the INT instruction on the provided interrupt vector. This triggers
 // a software interrupt. Templated since the instruction expects an
 // imm8.
-template <int N>
+template<int N>
 inline void INT() {
-  asm volatile("INT %0" ::"N"(N));
+  asm volatile("INT %0"::"N"(N));
 }
 
 // HLT calls the HLT instruction.
@@ -33,6 +33,13 @@ inline void HLT() { asm volatile("HLT"); }
 
 // CLI calls the CLI instruction.
 inline void CLI() { asm volatile("CLI"); }
+
+// RDTSC calls the rdtsc instruction. See https://www.felixcloutier.com/x86/rdtsc for more details.
+inline uint64_t RDTSC() {
+  uint32_t low, high;
+  asm volatile("RDTSC": "=a"(low), "=d"(high));
+  return static_cast<uint64_t>(low) | (static_cast<uint64_t>(high) << 32);
+}
 
 }  // namespace instructions
 
